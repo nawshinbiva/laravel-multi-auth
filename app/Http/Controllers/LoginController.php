011f683b79df53b75278658d44865ae4e9de94ec
Nawshin Nasir <nawshin22nasir@gmail.com>
2024-06-22 22:29:58 +0600

    dashboard added

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
@@ -21,10 +22,21 @@ class LoginController extends Controller
         ]);
 
         if ($validator->passes()) {
+
+            if(Auth::attempt(['email'=>$request->email,'password'=>$request->password])){
+
+            }else{
+                return redirect()->route('account.login')->with('Either email or password is incorrect!');
+            }
+
         } else {
             return redirect()->route('account.login')
                 ->withInput()
                 ->withErrors($validator);
         }
+    }
+
+    public function dashboard(){
+        
     }
 }
